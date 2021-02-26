3e3a9c076c2fc35579e68590d5af4bffbce9d8f4
Nazar-Tm <nazarmscs@gmail.com>
2021-02-26 16:34:44 +0500

    Some Changes

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Docker-Hub! I have been seen  {} times.\n'.format(count)
+    return 'Hello from Docker-Hub! I have seen  {} times.\n'.format(count)
